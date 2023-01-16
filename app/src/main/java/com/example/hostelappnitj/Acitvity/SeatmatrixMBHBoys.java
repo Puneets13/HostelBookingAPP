b1ab1d8f9fb8f204f02b6817a890e285124217ee
Kiranjeet <heykiranthakur@gmail.com>
2023-01-16 20:25:36 +0530

    Layout of seat matrix created

--- app/src/main/java/com/example/hostelappnitj/Acitvity/SeatmatrixMBHBoys.java
+++ app/src/main/java/com/example/hostelappnitj/Acitvity/SeatmatrixMBHBoys.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.pm.ActivityInfo;
+import android.media.MediaRouter2;
 import android.os.Bundle;
 import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
@@ -15,14 +17,17 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
+import com.alexvasilkov.gestures.Settings;
+import com.alexvasilkov.gestures.views.interfaces.GestureView;
 import com.example.hostelappnitj.R;
 
 public class SeatmatrixMBHBoys extends AppCompatActivity {
 AppCompatButton room301;
 //ScrollView scrollView ;
-    private float mScale = 1f;
-    private ScaleGestureDetector mScaleGestureDetector;
-    GestureDetector gestureDetector;
+//    private float mScale = 1f;
+//    private ScaleGestureDetector mScaleGestureDetector;
+//    GestureDetector gestureDetector;
+    GestureView gestureView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,50 +42,63 @@ AppCompatButton room301;
         });
 
 //    zoom in the ScrollView
-        gestureDetector = new GestureDetector(this, new GestureListener());
-
-        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleGestureDetector.SimpleOnScaleGestureListener(){
-            @Override
-            public boolean onScale(ScaleGestureDetector detector) {
-                float scale = 1 - detector.getScaleFactor();
-                float prevScale = mScale;
-                mScale += scale;
-
-                if (mScale > 10f)
-                    mScale = 10f;
-
-                ScaleAnimation scaleAnimation = new ScaleAnimation(1f / prevScale, 1f / mScale, 1f / prevScale, 1f / mScale, detector.getFocusX(), detector.getFocusY());
-                scaleAnimation.setDuration(0);
-                scaleAnimation.setFillAfter(true);
-//                ScrollView layout = (ScrollView) findViewById(R.id.scrollView);
-                LinearLayout layout1=(LinearLayout)findViewById(R.id.linearMatrix);
-                layout1.startAnimation(scaleAnimation);
-                return true;
-            }
-        });
-
+//        gestureDetector = new GestureDetector(this, new GestureListener());
+//
+//        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleGestureDetector.SimpleOnScaleGestureListener(){
+//            @Override
+//            public boolean onScale(ScaleGestureDetector detector) {
+//                float scale = 1 - detector.getScaleFactor();
+//                float prevScale = mScale;
+//                mScale += scale;
+//
+//                if (mScale > 10f)
+//                    mScale = 10f;
+//
+//                ScaleAnimation scaleAnimation = new ScaleAnimation(1f / prevScale, 1f / mScale, 1f / prevScale, 1f / mScale, detector.getFocusX(), detector.getFocusY());
+//                scaleAnimation.setDuration(0);
+//                scaleAnimation.setFillAfter(true);
+////                ScrollView layout = (ScrollView) findViewById(R.id.scrollView);
+//                LinearLayout layout1=(LinearLayout)findViewById(R.id.linearMatrix);
+//                layout1.startAnimation(scaleAnimation);
+//                return true;
+//            }
+//        });
+        gestureView=findViewById(R.id.gestureview);
+        gestureView.getController().getSettings()
+                .setMaxZoom(2f)
+                .setDoubleTapZoom(-1f) // Falls back to max zoom level
+                .setPanEnabled(true)
+                .setZoomEnabled(true)
+                .setDoubleTapEnabled(true)
+                .setRotationEnabled(true)
+                .setRestrictRotation(false)
+                .setOverscrollDistance(0f, 0f)
+                .setOverzoomFactor(2f)
+                .setFillViewport(false)
+                .setFitMethod(Settings.Fit.INSIDE)
+                .setGravity(Gravity.CENTER);
 
 
     }
 //to zoom the scrollView when touched
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent event) {
-        super.dispatchTouchEvent(event);
-        mScaleGestureDetector.onTouchEvent(event);
-        gestureDetector.onTouchEvent(event);
-        return gestureDetector.onTouchEvent(event);
-    }
-
-    private class GestureListener extends GestureDetector.SimpleOnGestureListener{
-        @Override
-        public boolean onDown(MotionEvent e) {
-            return true;
-        }
-
-        @Override
-        public boolean onDoubleTap(MotionEvent e) {
-            return true;
-        }
-    }
+//    @Override
+//    public boolean dispatchTouchEvent(MotionEvent event) {
+//        super.dispatchTouchEvent(event);
+//        mScaleGestureDetector.onTouchEvent(event);
+//        gestureDetector.onTouchEvent(event);
+//        return gestureDetector.onTouchEvent(event);
+//    }
+//
+//    private class GestureListener extends GestureDetector.SimpleOnGestureListener{
+//        @Override
+//        public boolean onDown(MotionEvent e) {
+//            return true;
+//        }
+//
+//        @Override
+//        public boolean onDoubleTap(MotionEvent e) {
+//            return true;
+//        }
+//    }
 
 }
