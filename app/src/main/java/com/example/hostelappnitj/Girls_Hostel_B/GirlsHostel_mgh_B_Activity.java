5d67819d05d0c900fc08f2831f614b60c8774e27
Puneet Singh <sidhupuneet115@gmail.com>
2023-09-28 19:45:28 +0530

    mgh-9_updated

---
+++ app/src/main/java/com/example/hostelappnitj/Girls_Hostel_B/GirlsHostel_mgh_B_Activity.java
@@ -0,0 +1,16 @@
+package com.example.hostelappnitj.Girls_Hostel_B;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hostelappnitj.R;
+
+public class GirlsHostel_mgh_B_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_girls_hostel_mgh_bactivity);
+    }
+}
---
+++ app/src/main/java/com/example/hostelappnitj/Girls_Hostel_B/mgh_B_floor_5.java
@@ -0,0 +1,16 @@
+package com.example.hostelappnitj.Girls_Hostel_B;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hostelappnitj.R;
+
+public class mgh_B_floor_5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mgh_b_floor5);
+    }
+}
